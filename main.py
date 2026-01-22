49af50d95acb5308d943b612e1b1f2dbd765d95c
Janvi9090 <maharnawarjanvi@gmail.com>
2026-01-22 22:37:17 +0530

    Data Processing

--- main.py
+++ main.py
@@ -9,6 +9,32 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from sklearn.metrics.pairwise import cosine_similarity
 
+import warnings
+warnings.filterwarnings('ignore')
+
 data = pd.read_csv('online_retail.csv')
 print(data.shape)
-print(data.columns)
+# print(data.columns)
+
+print("Missing values: ")
+print(data.isnull().sum())
+data = data.dropna(subset=['Description'])
+data = data.dropna(subset=['CustomerID'])
+
+print(data.shape)
+print("Missing values: ")
+print(data.isnull().sum())
+
+# Remove cancelled orders
+data = data[~data['InvoiceNo'].astype(str).str.startswith('C')]
+#Remove invalid quantity and price
+data = data[(data['Quantity']>0) & (data['UnitPrice']>0)]
+#Fix data types
+data['InvoiceDate'] = pd.to_datetime(data['InvoiceDate'])
+data['CustomerID'] = data['CustomerID'].astype(int)
+
+
+
+
+
+
